78fb8eef6da5d0b4a2656ade4786156dc83e7fd3
OlivlfilipeOliveira <filipeo444@gmail.com>
2021-04-23 22:25:47 -0300

    parte final

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,7 +4,7 @@ let connectionInSupport = [];
 
 socket.on("admin_list_all_users", connections => {
   connectionsUsers = connections;  
-  document.getElementById("list_users").innerHTML = '';
+  document.getElementById("list_users").innerHTML = "";
   
     let template = document.getElementById("template").innerHTML;
   
@@ -36,29 +36,29 @@ socket.on("admin_list_all_users", connections => {
       user_id: connection.user_id
     }
 
-    socket.emit("admin_list_messages_by_user", params, (messages) => {
-      const divMessages = document.getElementById(
-        `allMessages${connection.user_id}`
-      );
+    socket.emit('admin_user_in_support', params);
+
+    socket.emit('admin_list_messages_by_user', params, messages => {
+      const divMessages = document.getElementById(`allMessages${connection.user_id}`);
   
-      messages.forEach((message) => {
-        const createDiv = document.createElement("div");
+      messages.forEach(message => {
+        const createDiv = document.createElement('div');
   
         if (message.admin_id === null) {
-          createDiv.className = "admin_message_client";
+          createDiv.className = 'admin_message_client';
   
           createDiv.innerHTML = `<span>${connection.user.email} </span>`;
           createDiv.innerHTML += `<span>${message.text}</span>`;
           createDiv.innerHTML += `<span class="admin_date">${dayjs(
-            message.created_at
-          ).format("DD/MM/YYYY HH:mm:ss")}</span>`;
+            message.created_at,
+          ).format('DD/MM/YYYY HH:mm:ss')}</span>`;
         } else {
-          createDiv.className = "admin_message_admin";
+          createDiv.className = 'admin_message_admin';
   
           createDiv.innerHTML = `Atendente: <span>${message.text}</span>`;
           createDiv.innerHTML += `<span class="admin_date>${dayjs(
-            message.created_at
-          ).format("DD/MM/YYYY HH:mm:ss")}`;
+            message.created_at,
+          ).format('DD/MM/YYYY HH:mm:ss')}`;
         }
   
         divMessages.appendChild(createDiv);
@@ -89,10 +89,10 @@ socket.on("admin_list_all_users", connections => {
   }
 
   socket.on("admin_receive_message", (data) => {
-    console.log(data);
+    
     const connection = connectionInSupport.find(
-      (connection) => connection.socket_id === data.socket_id
-    ); 
+      connection => (connection.socket_id === data.socket_id),
+    );
   
     const divMessages = document.getElementById(
       `allMessages${connection.user_id}`
@@ -106,6 +106,6 @@ socket.on("admin_list_all_users", connections => {
     createDiv.innerHTML += `<span class="admin_date">${dayjs(
       data.message.created_at
     ).format("DD/MM/YYYY HH:mm:ss")}</span>`;
-  
+      console.log(data);
     divMessages.appendChild(createDiv);
   });
